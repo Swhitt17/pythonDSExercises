ae1281ba218657990edb7c4c6bd445b8b67c15a4
Sara Whittingslow <swhittings17@gmail.com>
2024-01-04 13:20:37 -0500

    added python ds exercises

---
+++ vowel_count.py
@@ -0,0 +1,19 @@
+VOWELS = "aeiou"
+def vowel_count(phrase):
+    """Return frequency map of vowels, case-insensitive.
+
+        >>> vowel_count('rithm school')
+        {'i': 1, 'o': 2}
+        
+        >>> vowel_count('HOW ARE YOU? i am great!') 
+        {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
+    """
+
+    phrase = phrase.lower()
+    vowel_amount = {}
+
+    for char in phrase:
+        if char in VOWELS:
+           vowel_amount[char] = vowel_amount.get(char,0) + 1
+           
+    return vowel_amount
